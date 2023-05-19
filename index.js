aa33243bb8327a9b2f3a3f15bc1f4c55ea0e764b
rorroalejandro <rorrosb19@gmail.com>
2023-05-19 18:34:46 -0400

    Codigo actualizado nuevo proyecto tiendazapatillas

--- index.js
+++ index.js
@@ -1,41 +1,211 @@
 
 
-alert("!Bienvenido a Simucuot tu simulador de cuotas en linea!");
+alert("!Bienvenido a TusTillas!");
+
+let carrito = []
 
 
-
-function obtenerMonto() {
-    let monto = prompt("Ingresa el monto a calcular ");
-    if (isNaN(monto)){
-        alert("El valor ingresado debe ser un numero");        
-        return obtenerMonto();
+const saludar = function() {
+    let nombre = prompt("Ingresa tu nombre y apellido:")
+    if (!isNaN(nombre)) { 
+        alert("Debes ingresar tu nombre sin numeros")
+        saludar()    
     } else {
-        return parseInt(monto); 
-    }  
-} 
-
-
-function obtenerCuotas() {
-    let cuotas = prompt("Numero de cuotas a calcular");
-    if (isNaN(cuotas)){
-        alert("El valor ingresado debe ser un numero");        
-        return obtenerMonto();
-    } else {
-        return parseInt(cuotas);
-    } 
-}
-
-
-function calcularMontoCuotas(monto, cuotas) {
-    let montoCuota = monto / cuotas;
-    for (let i = 0; i < cuotas; i++) {
-        console.log("cuota mes " + (i + 1) + " es de " + montoCuota.toFixed(2));
+        return alert(`¡Bienvenido ${nombre}! Estás en tu tienda de zapatillas online.`);    
     }
-    return montoCuota.toFixed(2);
+ 
+}
+saludar() 
+
+
+
+// objetos (zapatillas) de la tienda
+// array de objetos (zapatillas)
+
+let zapatillas = [{
+    marca: "Nike",
+    color: "Blanco",
+    talla: 41,
+    stock: 12,
+    valor: 50000,
+    id: 1
+}, {
+    marca: "Adidas",
+    color: "negro",
+    talla: 42,
+    stock: 6,
+    valor: 40000,
+    id: 2
+}, {
+    marca: "DC",
+    color: "plomo",
+    talla: 38,
+    stock: 8,
+    valor: 35000,
+    id: 3
+}, {
+    marca: "Puma",
+    color: "negro",
+    talla: 40,
+    stock: 10,
+    valor: 29000,
+    id: 4
+}, {
+    marca: "Nike",
+    color: "negro",
+    talla: 38,
+    stock: 15,
+    valor: 40000,
+    id: 5
+}, {
+    marca: "Adidas",
+    color: "Blanco",
+    talla: 41,
+    stock: 7,
+    valor: 37000,
+    id: 6
+}, {
+    marca: "Puma",
+    color: "Blanco",
+    talla: 41,
+    stock: 2,
+    valor: 23000,
+    id: 7
+}, {
+    marca: "DC",
+    color: "blanco",
+    talla: 41,
+    stock: 9,
+    valor: 34000,
+    id: 8
+}, {
+    marca: "DC",
+    color: "negro",
+    talla: 42,
+    stock: 14,
+    valor: 26000,
+    id: 9
+}, {
+    marca: "Nike",
+    color: "rojo",
+    talla: 39,
+    stock: 4,
+    valor: 38000,
+    id: 10
+}, {
+    marca: "Adidas",
+    color: "plomo",
+    talla: 41,
+    stock: 20,
+    valor: 44000,
+    id: 11
+}, {
+    marca: "Puma",
+    color: "Blanco",
+    talla: 42,
+    stock: 20,
+    valor: 36000,
+    id: 12
+
+}];
+
+
+
+
+
+console.log(zapatillas.length);
+
+
+
+// bucle que muestra en alert el listado de zapatillas disponibles  
+let marcaZapatilla = "";
+
+for (let i = 0; i < zapatillas.length; i++) {
+    marcaZapatilla += `Marca: ${zapatillas[i].marca}, Color: ${zapatillas[i].color}, Talla: ${zapatillas[i].talla}\n`;
 }
 
-let monto = obtenerMonto();
-let cuotas = obtenerCuotas();
-let montoCuota = calcularMontoCuotas(monto, cuotas);
+alert(`las zapatillas que tenemos son: \n${marcaZapatilla} `);
 
-alert("El monto de cada cuota es de " + montoCuota + " Por un periodo de " + cuotas + "meses");
+
+// esta funcion se ejecuta automaticamente despues de mostrar las zapatillas disponibles
+function verZapatillas() {
+    let consultaMarca = prompt("De las zapatillas vistas anteriormente... que marca te interesa??")
+    let zapatillasEncontradas = [];  
+   
+
+    // bucle para traer desde el array de objetos la zapatilla segun la marca que le interese al cliente
+    for (let i = 0; i < zapatillas.length; i++) {
+        if (zapatillas[i].marca.toLowerCase() == consultaMarca.toLowerCase()) { // se le asigna tolowercase para facilitar la busqueda de caracteres iguales
+           zapatillasEncontradas.push(zapatillas[i]);  //usando el metodo push se agregan todas las coincidencias al array vacio
+        } 
+    };
+
+    
+    // se crea condicion si el nuevo array de zapatillas es mayor a 0 se recorre con un bucle para enlistar en un alert los modelos encontrados
+    if (zapatillasEncontradas.length > 0) {
+        let resultado = `Zapatillas encontradas: \n`;
+        for (let i = 0; i < zapatillasEncontradas.length; i++) {
+            resultado += `Indice: ${zapatillasEncontradas[i].id} Marca: ${zapatillasEncontradas[i].marca} Color: ${zapatillasEncontradas[i].color} Talla: ${zapatillasEncontradas[i].talla} Stock: ${zapatillasEncontradas[i].stock} Valor$: ${zapatillasEncontradas[i].valor} \n`
+        };
+
+        let respuesta = confirm(`${resultado}Quieres buscar otros modelos?`); // se agrega confirm para preguntar si el cliente quiere consultar otro modelo
+        console.log(152,respuesta);
+
+        
+        if (respuesta) {
+            verZapatillas()
+        } else {
+            console.log("paso por aqui")
+            cotizaZapatillas() // no funciona la condicion false!!!!!!!!!!
+        }
+    } else {
+        alert(`No se encontro el modelo indicado`);
+        verZapatillas();
+    }
+
+    
+}
+verZapatillas(); 
+
+function calcularCarrito (){
+    let suma = 0
+
+    for (i = 0; i < carrito.length; i++) {
+        suma += carrito[i].valor 
+    };
+    alert(`El total de tu carrito de compra es $: ${suma}`)
+}
+
+function cotizaZapatillas() {
+
+    let consultaIndice = parseInt(prompt(`Indica el indice de la zapatilla que te interesa`));
+    
+
+    if (consultaIndice <= 12) {
+
+        for (i = 0; i < zapatillas.length; i ++) {
+            if (consultaIndice == zapatillas[i].id) {
+                carrito.push(zapatillas[i])
+                console.table(carrito)
+            }
+        }
+        let continuar = confirm("Desea seguir agregando zapatillas al carrito??")
+        console.log(continuar)
+        if (continuar) {
+            console.log("miga de pan")
+            cotizaZapatillas()   
+        } else {
+            calcularCarrito()
+        }
+
+    } else {
+        alert("El id ingresado es invalido")
+        cotizaZapatillas()
+    };
+}
+
+
+
+
+// crear  funcion para sumar objetos en variable tienda
+// enumerar zapatillas mediante indice
